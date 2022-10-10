c450c2992084d136e157c198f28194f0d4e60d34
Joe Pigott (Windows) <a02366914@aggies.usu.edu>
2022-10-10 05:15:17 -0600

    added sparse explanation. do it tommorrow.

---
+++ src/main.py
@@ -0,0 +1,5 @@
+# The potential felt by an atom in a sodium-chloride lattice is
+#                     e     
+# V = ---------------------------------
+#     4 pi ep_0 a sqrt(i^2 + j^2 + k^2)
+# where a is the space between the atoms in the lattice. Sodium atoms are present when (i + j + k) % 2 = 0 , and hold a charge of e, while chlorine atoms are present when (i + j + k) % 2 = 1, and hold a charge of -e.
